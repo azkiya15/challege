47c7a852a5cb76b8f4b4da8a764c66457174fca8
azkiya15 <kiyaamdn@gmail.com>
2020-08-25 21:52:56 +0700

    challenge 2 selesai

--- challenge2.js
+++ challenge2.js
@@ -1,29 +1,21 @@
-function deretKaskus(val){
-    let array = [];
-    let i;
-    for(i=3; i <= val*3; i+=3){
-        //  console.log(i);
-        if(i%5 == 0 && i%6 == 0){
-           array.push("kaskus"); 
-        }else if(i%5 == 0){
-           array.push("kas");    
-        }else if(i%6 == 0){
-            array.push("kus");
-        }      
-        else {
-           array.push(i);
+function deretKaskus(val) {
+  let array = [];
+  let i;
+  for (i = 3; i <= val * 3; i += 3) {
+    //  console.log(i);
+    if (i % 5 == 0 && i % 6 == 0) {
+      array.push("kaskus");
+    } else if (i % 5 == 0) {
+      array.push("kas");
+    } else if (i % 6 == 0) {
+      array.push("kus");
+    } else {
+      array.push(i);
+    }
 
-        }  
-    
-        
-        
-        
-        
-        //  console.log(i);
-    } 
-    return array;
-
-
+    //  console.log(i);
+  }
+  return array;
 }
 
 console.log(deretKaskus(10));

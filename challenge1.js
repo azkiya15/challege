1e0dfa4f7c216e9d6338fb8d031b4ca3a5c3193f
azkiya15 <kiyaamdn@gmail.com>
2020-08-25 18:41:01 +0700

    fix challenge 1

---
+++ challenge1.js
@@ -0,0 +1,25 @@
+function sum1(a,b,c){
+    return a + b + c;
+}
+const hasilsum1 = sum1(1,2,7);
+console.log(hasilsum1);
+
+
+function sum2(d,e){
+    return d+e;
+}
+const hasilsum2 = sum2(1,4);
+console.log(hasilsum2);
+
+
+function sum3(f){
+    return f;
+}
+const hasilsum3 = sum3(11);
+console.log(hasilsum3);
+
+function sum4(g,h,i,j,k){
+    return g+h+i+j+k;
+}
+const hasilsum4 = sum4(10,3,6,7,9);
+console.log(hasilsum4);

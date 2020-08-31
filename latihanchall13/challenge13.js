6bdaae98821e6c694c0984c3b52894acdab8bf4d
azkiya15 <kiyaamdn@gmail.com>
2020-08-31 22:14:54 +0700

    done

--- latihanchall13/challenge13.js
+++ latihanchall13/challenge13.js
@@ -1,84 +1,3 @@
-// // process.argv.forEach((val,index) => {
-//   //     console.log(`${index}: ${vall}`);
-//   // })
-//   // let student = {
-//     "use strict";
-
-//     var fs = require("fs");
-
-//     let jsonData = require('./example.json')
-//   //   listTodo: process.argv[3],
-//   // };
-
-//   // let data = JSON.stringify(jsonData);
-// if (process.argv[2]) {
-//   switch (process.argv[2]) {
-//     case "list":
-//       // console.log("List");
-//       // console.log(process.argv[3]);
-
-//       fs.readFile("example.json", (err, data) => {
-//         if (err) throw err;
-//         let tampil = JSON.parse(data);
-//         console.log(tampil);
-//       });
-
-//       console.log("List");
-//       break;
-
-//     case "add":
-
-//       // fs.writeFileSync("student-2.json", data);
-//     fs.writeFile('student-2.json',data,
-//     // callback function that is called after writing file is done
-//     function(err) {
-//         if (err) throw err;
-//         // if no error
-//         console.log("Data is written to file successfully.")
-// });
-// //  data.push(process.argv[3]);
-
-//       // let tambah= {
-//       //   task = "belajar",
-//       //  complete = false,
-//       //  tags = []
-//       // }
-//       // fs.readFile(file, 'utf8', function(err, data){
-
-//         if(err){
-//             return callback(err, null);
-//         }
-
-//       //   var json;
-
-// fs.writeFile("example.json", (err, data) => {
-//   if (err) throw err;
-//   data.push(tambah)
-// });
-
-//       // let change = JSON.stringify(tambah);
-//       // tampil.push(change);
-
-// // let obj = JSON.parse(data);
-// // obj.push(student);
-// // jsonStr = JSON.stringify(obj);
-//       break;
-
-//     case "delete":
-//       fs.unlink("student-2.json", data);
-//       console.log("delete success.");
-//       break;
-
-//   }
-// } else {
-//   let daftarTodo = `
-//     To do
-//     1. List
-//     2. Add
-//     3. Delete`;
-//   console.log(daftarTodo);
-// }
-
 "use strict";
 
 var fs = require("fs");
@@ -87,27 +6,203 @@ let jsonData = require("./example.json");
 if (process.argv[2]) {
   switch (process.argv[2]) {
     case "list":
-      // console.log("List");
-      // console.log(process.argv[3]);
-
+      
+      console.log("\nList yg harus dikerjakan hari ini :");
       fs.readFile("example.json", "utf8", (err, data) => {
         if (err) throw err;
         let tampil = JSON.parse(data); //convert string to object
-        // console.log(tampil);
-        let tambah = {
-          task: "main",
-          complete: false,
-          tags: [],
-        };
-
-        tampil.push(tambah);
-        // console.log(tampil);
-        let change = JSON.stringify(tampil) //mengubah objek menjadi string
-        fs.writeFileSync('example.json', JSON.stringify(tampil, null, 3))
-        console.log("Saved");
-      });
-
-      console.log("List");
+        //  console.log(tampil);
+        
+        for(let i=0; i<tampil.length; i++){
+          if(null){
+            console.log("");
+          }else{
+          if(tampil[i].complete==true){
+            console.log("[x]", tampil[i].task);
+          }else{
+            console.log("[]", tampil[i].task);
+          }
+    
+        }
+        }
+      })
       break;
+
+      case "add":
+        let argvArray = process.argv
+        // console.log("cek ", argvArray);
+       
+          let kalArray = [];
+          for(let i=3; i<argvArray.length; i++){
+            kalArray.push(argvArray[i])
+          }
+        let kalimat = kalArray.join(" ")
+          // console.log(kalimat);
+
+        fs.readFile("example.json", "utf8", (err, data) => {
+          if (err) throw err;
+          let tampil = JSON.parse(data); //convert string to object //its array tampil[]
+          // console.log(tampil);
+
+
+          let tambah = {
+            task: kalimat,
+            complete: false,
+            tags: [],
+          };
+
+
+  
+          tampil.push(tambah);
+          // console.log(tampil);
+
+         //mengubah objek menjadi string dgn JSON.stringify
+          fs.writeFileSync('example.json', JSON.stringify(tampil, null, 3))
+          console.log("Saved");
+        });
+  
+        console.log("List");
+        break;
+        
+        case "delete":
+          
+        let hapus = process.argv[3];
+          fs.readFile("example.json", "utf8", (err, data) => {
+            if (err) throw err;
+            let tampil = JSON.parse(data);
+            
+            Array.prototype.remByVal = function(val) {
+              for (var i = 0; i < this.length; i++) {
+                  if (this[i] === val) {
+                      this.splice(i, 1);
+                      i--;
+                  }
+              }
+              return this;
+          }
+          
+          
+          tampil = tampil.remByVal(tampil[hapus])
+          
+            
+            fs.writeFileSync('example.json', JSON.stringify(tampil, null, 3))
+            console.log(`List ${hapus} has been deleted.`);
+
+          })
+
+          break;
+          
+          case "complete" :
+
+          fs.readFile("example.json", "utf8", (err, data) =>{
+            if (err) throw err;
+              let tampil = JSON.parse(data)
+
+              let index = process.argv[3]
+              if(tampil[index].complete==true){
+                console.log("task has been done");
+              }else{
+                tampil[index].complete = true;
+                fs.writeFileSync('example.json', JSON.stringify(tampil, null, 3))
+                console.log(`OK`);
+              }
+  
+          })
+
+          break;
+
+          case "uncomplete":
+            
+            fs.readFile("example.json", "utf8", (err, data) => {
+              let tampil = JSON.parse(data)
+              let index = process.argv[3]
+              
+              if(tampil[index].complete==false){
+                console.log("Tugas memang belum selesai");
+              }else{
+                tampil[index].complete = false;
+                console.log(`OK`);
+                fs.writeFileSync('example.json', JSON.stringify(tampil, null, 3))
+              }
+
+            })
+          break;
+          
+          case "task":
+
+            fs.readFile("example.json", "utf8", (err, data) => {
+              let tampil = JSON.parse(data);
+
+              let index = process.argv[3]
+              let kataTambah = process.argv[4]
+              
+              tampil[index].tags.push(kataTambah)
+              console.log("OK");
+
+              fs.writeFileSync('example.json', JSON.stringify(tampil, null, 3))
+            })
+  
+          break;
+
+          case "belum":
+          
+          fs.readFile('example.json', 'utf8', (err, data) => {
+            let tampil = JSON.parse(data)
+
+            for(let i=0; i<tampil.length; i++){
+              if(tampil[i].complete == false){
+                console.log("[]",tampil[i].task);
+              }
+            }
+          })
+
+          break;
+
+          case "udah":
+          fs.readFile('example.json', 'utf8', (err, data) => {
+            let tampil = JSON.parse(data)
+
+            for(let i=0; i<tampil.length; i++){
+              if(tampil[i].complete == true){
+                console.log("[x]", tampil[i].task);
+              }
+            }
+          })
+          break;
+
+          case "filter":
+             
+          fs.readFile('example.json', 'utf8', (err, data) => {
+            let tampil = JSON.parse(data)
+
+
+
+            function filterByValue(tampil, term) {
+              var ans = tampil.filter(function(v,i) {
+                  if(v.task.toLowerCase().indexOf(term) >=0) {
+                      return true;
+                  } else false;
+              });
+              console.log( ans);
+          }
+          filterByValue(tampil, process.argv[3]);
+            
+            
+            
+            // for(let i=0; i<tampil.length; i++){
+            //   if(tampil[i].task == kataCari){
+            //     if(tampil[i].complete==true){
+            //       let udah = "[x] ";
+            //       console.log(udah, tampil[i].task);
+            //     }else{
+            //       let belum = "[] ";
+            //       console.log(belum, tampil[i].task);
+            //     }
+            //   }
+            // }
+          })  
+          break;
   }
+}else{
+  console.log("done");
 }

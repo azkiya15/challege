b9ee18efec3c64ee3f0fe211330c16bfe54eab6f
azkiya15 <kiyaamdn@gmail.com>
2020-09-16 19:52:07 +0700

    done

--- challenge20/routes/index.js
+++ challenge20/routes/index.js
@@ -32,7 +32,7 @@ router.get("/", function (req, res, next) {
       throw err;
     }
     // console.log(sql);
-    res.render("index", {  model: rows });
+    res.render("/", { model: rows });
     });
   });
   
@@ -42,114 +42,113 @@ router.get("/add", function (req, res, next) {
 })
 
 router.get("/editt/:id", function (req, res, next) {
-  const userId = req.params["id"];
+  // const userId = req.params["id"];
+  let sql = `SELECT * FROM bread`
 
+  db.all(sql, [], (err, rows) => {
+    for(let i=0; i<rows.length; i++){
+      let sql = `SELECT id, string, integer, float, date, boolean FROM bread WHERE id = ?`
+      const userId = req.params["id"];
 
-
-  fs.readFile("data.json", "utf8", (err, data) => {
-    if (err) throw err;
-    let tampil = JSON.parse(data);
-    let show
-    for(let i=0; i<tampil.length; i++){
-      if(tampil[i].id == userId){
-        // console.log("CEK", tampil[i])
-         show = {
-          id: tampil[i].id,
-            string: tampil[i].string,
-            integer: tampil[i].integer,
-            float: tampil[i].float,
-            date: tampil[i].date,
-            boolean: tampil[i].boolean
+      db.get(sql, [userId], (err, row) => {
+        if (err) {
+          return console.error(err.message);
         }
-      }
-    } console.log("CEK", show);
-    res.render("editt", { title: "Express", content: show });
         
-
+          res.render("editt", { title: "Express", content: row });
+      })
+    }
   })
+        
 
 });
 
 
 router.post('/submitadd', (req, res) => {
- 
-  //get the new user data from post request
-  const userData = req.body
-  let tampil, tambah;
-  fs.readFile("data.json", "utf8", (err, data) => {
-    let tampil = JSON.parse(data)
+  // let idtinggi = "SELECT max(id) FROM bread"
+  // const idbaru = idtinggi + 1;
+  // const sql = "INSERT INTO bread (string, integer, float, date, boolean) VALUES (?, ?, ?, ?, ?)";
+  // const book = [idbaru, req.body.string, req.body.integer, req.body.float, req.body.date, req.body.boolean];
+  // db.run(sql, book, err => {
+  //   // if (err) ...
+  //   res.render("index", { content: book });
+  // });
+  let idtinggi = "SELECT max(id) FROM bread"
+  const idbaru = idtinggi + 1;
+    // let string = req.body.string;
+    // let integer = req.body.integer;
+    // let float = req.body.float;
+    // let date = req.body.date;
+    // let boolean = req.body.boolean;
+    // let errors = false;
 
-    for(let i=0; i<tampil.length; i++){
-      num = tampil.length + 1;
-       tambah = {
-        id: num.toString(),
-        string: userData.string,
-        integer: userData.integer,
-        float: userData.float,
-        date: userData.date,
-        boolean: userData.boolean,
-      }
-      
-    }
+    
+    // // if no error
+    // if(!errors) {
 
-    tampil.push(tambah)
+    //     var form_data = {
+    //         id: idbaru,
+    //         string: string,
+    //         integer: integer,
+    //         float: float,
+    //         date: date,
+    //         boolean: boolean
+    //     }
+        
+    //     // insert query
+    //     db.query('INSERT INTO bread SET ?', form_data, function(err, result) {
+    //         //if(err) throw err
+    //         if (err) {
+    //             req.flash('error', err)
+                 
+    //             // render to add.ejs
+    //             res.render('/add', {
+    //               id: idbaru,
+    //                 string: form_data.string,
+    //                 integer: form_data.integer,
+    //                 float: form_data.float,
+    //                 date: form_data.date,
+    //                 boolean: form_data.boolean                    
+    //             })
+    //         } else {                
+    //             req.flash('success', 'Book successfully added');
+    //             res.redirect('/index');
+    //         }
+    //     })
+    // }
 
-    const stringifyData = JSON.stringify(tampil, null, 3)
-    fs.writeFileSync('data.json', stringifyData)
-    res.render("index", { title: "Express", content: tampil });
+    const book = [req.body.string, req.body.integer, req.body.float, req.body.date, req.body.boolean];
+    const sql = "INSERT INTO bread(string, integer, float, date, boolean) VALUES(?, ?, ?, ?, ?)";
+    db.run(sql, book, err => {
+      // if (err) ...
+      res.render("index");
+    });
 
-  })
 })
 
 
 
 router.post("/submiteditt/:id", (req, res) => {
-  const dataBody = req.body
-  // console.log("body", dataBody);
-  const userId = req.params["id"];
+  
+  const id = req.params.id;
+  const book = [req.body.string, req.body.integer, req.body.float, req.body.date, req.body.boolean, id];
+  const sql = "UPDATE bread SET string = ?, integer = ?, float = ?, date = ?, boolean = ? WHERE (id = ?)";
+  db.run(sql, book, err => {
+    // if (err) ...
+    res.redirect("index");
+  });
 
-
-
-  fs.readFile("data.json", "utf8", (err, data) => {
-    let tampil = JSON.parse(data)
-    for(let i=0; i<tampil.length; i++){
-      if(tampil[i].id == userId){
-        tampil[i].id = userId;
-        tampil[i].string = dataBody.string;
-        tampil[i].integer = dataBody.integer;
-        tampil[i].float = dataBody.float;
-        tampil[i].date = dataBody.date;
-        tampil[i].boolean = dataBody.boolean;
-      }
-    }
-    const stringifyData = JSON.stringify(tampil, null, 3)
-    fs.writeFileSync('data.json', stringifyData)
-    res.render("index", { title: "Express", content: tampil });
-  })
 });
 
 router.get("/submitdel/:id", (req, res) => {
-  const userId = req.params["id"];
-  fs.readFile("data.json", "utf8", (err, data) => {
-    if (err) throw err;
-    let tampil = JSON.parse(data);
-    for(let i=0; i<tampil.length; i++){
-      if(tampil[i].id === userId){
-        tampil.splice(i, 1);
-              i--;
-        num = Number(tampil[i].id)
-        if(num > userId){
-          num = num + 1;
-          tampil[i].id = num;
-        }
-      }
+  const id = req.params.id;
+  const sql = "DELETE FROM bread WHERE id = ?";
+  db.run(sql, id, err => {
+    if (err) {
+      return console.error(err.message);
     }
-    const stringifyData = JSON.stringify(tampil, null, 3)
-    fs.writeFileSync('data.json', stringifyData)
-    res.render("index", { title: "Express", content: tampil });
-
-  })
-
+    res.redirect("/");
+  });
 });
 
 
